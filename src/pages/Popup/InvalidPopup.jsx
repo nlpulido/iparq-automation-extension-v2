8fbe52d79b85f0b0f1f0f34f6adb7832f92baf32
Neil Pulido <neilpphoto@gmail.com>
2021-06-04 11:09:10 -0700

    added conditional rendering

---
+++ src/pages/Popup/InvalidPopup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Popup.css';
+
+const InvalidPopup = () => {
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h1>Please use the extension on the iPARQ Admin site to access the utilities.</h1>
+            </div>
+        </div>
+    );
+}
+
+export default InvalidPopup;
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Popup from './Popup';
+import InvalidPopup from "./InvalidPopup";
 import './index.css';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+// check if we're on an iPARQ URL
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+
+    // rewrite popup.html if we're not on an admin iPARQ site
+    if (!tabs[0].url.includes("admin.thepermitstore")) {
+        render(<InvalidPopup />, window.document.querySelector('#app-container'));
+    } else {
+        render(<Popup />, window.document.querySelector('#app-container'));
+    }
+
+});
 
 if (module.hot) module.hot.accept();
