19c873f5fe9c82e95b180587b1e8ed6b6b1d2828
Neil Pulido <neilpphoto@gmail.com>
2021-06-04 10:07:04 -0700

    changed the code to boiler plate code

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <h1>iPARQ Permit Validator</h1>
+        <select>
+          <option value="main">Main Portal</option>
+          <option value="affiliate">Affiliate Portal</option>
+        </select>
+        <div className="popup-buttons">
+          <button className="button-container" id="validateBtn">Validate Permits</button>
+          <button className="button-container" id="cancelBtn" disabled>Cancel</button>
+        </div>
+        <a href="https://docs.google.com/spreadsheets/d/1WOto59_8sdDg1_4Zd52UAteDmusdW1F5WdtBbGhpouk/" target="_blank" rel="noopener noreferrer" className="popup-link"><h3>Edit Google Sheet</h3></a>
       </header>
     </div>
   );
