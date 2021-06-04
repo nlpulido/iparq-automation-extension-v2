ae2e8961ec09562431e00b2844d599e5758ccf00
Neil Pulido <neilpphoto@gmail.com>
2021-06-04 11:19:39 -0700

    added a selector

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import React, { useState } from 'react';
+import Select from 'react-select';
 import './Popup.css';
 
+
+const portalOptions = [
+  { value: "main", label: "Main Portal" },
+  { value: "affiliate", label: "Affiliate Portal" }
+]
+
+function initializeDropdown() {
+
+  // query our current tab & set our dropdown accordingly
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs[0].title.includes("AFFILIATES")) {
+      document.getElementById("permit-portal").value = "affiliate";
+    }
+  });
+
+}
+
 const Popup = () => {
+  let [portal, setPortal] = useState("main");
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>iPARQ Permit Validator</h1>
-        <select>
-          <option value="main">Main Portal</option>
-          <option value="affiliate">Affiliate Portal</option>
-        </select>
+        <Select options={portalOptions} className="dropdown"/>
         <div className="popup-buttons">
           <button className="button-container" id="validateBtn">Validate Permits</button>
-          <button className="button-container" id="cancelBtn" disabled>Cancel</button>
         </div>
         <a href="https://docs.google.com/spreadsheets/d/1WOto59_8sdDg1_4Zd52UAteDmusdW1F5WdtBbGhpouk/" target="_blank" rel="noopener noreferrer" className="popup-link"><h3>Edit Google Sheet</h3></a>
       </header>
