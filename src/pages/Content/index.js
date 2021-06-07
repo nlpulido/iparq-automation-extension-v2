9d1ddacf2f4574329b9d63382787cf8436c9bdff
Neil Pulido <neilpphoto@gmail.com>
2021-06-07 11:18:33 -0700

    added actions to the buttons in order to enact the scripts

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,58 @@
 import { printLine } from './modules/print';
 
+// Promise to switch to the main portal
+function switchToMainPortal() {
+  return new Promise((resolve) => {
+    console.log("Switching to main portal");
+    // Navigate to the main portal
+    window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=83";
+    resolve();
+  });
+}
+
+// Promise to switch to the affiliate portal
+function switchToAffiliatePortal() {
+  return new Promise((resolve) => {
+    console.log("Switching to affiliate portal");
+    // Navigate to the main portal
+    window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=155";
+    resolve();
+  });
+}
+
+// promise to navigate to the permit types
+function navigatePermitTypes() {
+  return new Promise((resolve) => {
+      console.log("Switching to navigating to permit types");
+
+      // Navigate to the permit types page
+      window.location.href = "https://admin.thepermitstore.com/setup/permittypes.php";
+
+      resolve();
+  })
+}
+
+function startValidator() {
+  console.log("Starting validator...");
+  navigatePermitTypes();
+}
+
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
-
 printLine("Using the 'printLine' function from the Print Module");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.type == "main") {
+      switchToMainPortal();
+    } else if (request.type == "affiliate") {
+      switchToAffiliatePortal();
+    } else if (request.type == "interrupt") {
+      console.log("Interrupting validator...");
+    } else if (request.type == "start") {
+      startValidator();
+    } else {
+      console.log(`Got an unknown type: ${request.type}`)
+    }
+  }
+);
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -12,6 +12,19 @@ const Popup = () => {
 
   function handlePortalChange(portal) {
     setPortal(portal);
+
+    // send a message to the content script to change the portal
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: portal.value});
+    });
+
+  }
+
+  function startValidation() {
+    // send a message to the content script to change the portal
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: "start"});
+    });
   }
 
   return (
@@ -25,7 +38,7 @@ const Popup = () => {
           onChange={(selected) => handlePortalChange(selected)}
         />
         <div className="popup-buttons">
-          <button className="button-container" id="validateBtn">Validate Permits</button>
+          <button className="button-container" id="validateBtn" onClick={startValidation}>Validate Permits</button>
         </div>
         <a href="https://docs.google.com/spreadsheets/d/1WOto59_8sdDg1_4Zd52UAteDmusdW1F5WdtBbGhpouk/" target="_blank" rel="noopener noreferrer" className="popup-link"><h3>Edit Google Sheet</h3></a>
       </header>
