7fb8f008738ad200b8b316e6bc4fcde872219044
Neil Pulido <neilpphoto@gmail.com>
2021-06-08 11:01:42 -0700

    finally surveying the permits'

---
+++ src/pages/Content/automation.js
@@ -0,0 +1,8 @@
+function surveyPermitTypes() {
+    var permit_table = document.getElementById('st_setuppermittypes');
+    console.log(`permit_table: ${permit_table}`);
+}
+
+document.onLoad(() => {
+    console.log("This should appear on the permit types page!");
+})
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,45 +1,67 @@
-import { printLine } from './modules/print';
+// import { printLine } from './modules/print';
+// console.log('Content script works!');
+// console.log('Must reload extension for modifications to take effect.');
+// printLine("Using the 'printLine' function from the Print Module");
+
+const HOME = "https://admin.thepermitstore.com/home";
+const PERMIT_TYPES = "https://admin.thepermitstore.com/setup/permittypes.php";
 
 // Promise to switch to the main portal
 function switchToMainPortal() {
-  return new Promise((resolve) => {
-    console.log("Switching to main portal");
-    // Navigate to the main portal
-    window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=83";
-    resolve();
-  });
+  console.log("Switching to main portal");
+
+  // Navigate to the main portal
+  window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=83";
 }
 
 // Promise to switch to the affiliate portal
 function switchToAffiliatePortal() {
-  return new Promise((resolve) => {
-    console.log("Switching to affiliate portal");
-    // Navigate to the main portal
-    window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=155";
-    resolve();
-  });
+  console.log("Switching to affiliate portal");
+
+  // Navigate to the main portal
+  window.location.href = "https://admin.thepermitstore.com/setup/switch_client.php?table=switch&edit=155";
 }
 
 // promise to navigate to the permit types
 function navigatePermitTypes() {
-  return new Promise((resolve) => {
-      console.log("Switching to navigating to permit types");
+    console.log("Switching to navigating to permit types");
 
-      // Navigate to the permit types page
-      window.location.href = "https://admin.thepermitstore.com/setup/permittypes.php";
-
-      resolve();
-  })
+    // Navigate to the permit types page
+    window.location.href = "https://admin.thepermitstore.com/setup/permittypes.php";
 }
 
 function startValidator() {
   console.log("Starting validator...");
-  navigatePermitTypes();
+  // navigatePermitTypes();
 }
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-printLine("Using the 'printLine' function from the Print Module");
+if (window.location.href === HOME) {
+  navigatePermitTypes();
+} else if (window.location.href === PERMIT_TYPES) {
+  // find the permit tables
+  var permit_table_rows = document.getElementById('st_setuppermittypes').rows;
+
+
+  // loop through each row starting past the Do Not Ticket
+  for (var i = 3; i < permit_table_rows.length; i++) {
+    // grab the current row 
+    var curr_row = permit_table_rows.item(i);
+
+    // otherwise, log the inner text
+    var cells = curr_row.cells;
+    var permit_title = cells.item(0).innerText;
+
+    // if we encounter the row with the remove items, we break
+    if (permit_title === "Show Removed Items") {
+      break;
+    } else if (permit_title === "Do Not Ticket") {
+      continue;
+    }
+
+    console.log(permit_title);
+  }
+
+}
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
